c37cb6891c0712f7d7847b8833ab699e3c3b2378
Юля Кобзева <selemina@mail.ru>
2023-05-12 16:40:40 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
+        boolean isPalindrome = isPalindrome("Tenet");
+        System.out.println(isPalindrome);
+    }
+
+    public static boolean isPalindrome(String string) {
+        string = string.toLowerCase(); //приводит всю строку к нижнему регистру
+        StringBuilder builder = new StringBuilder();
+        builder.append(string);
+        builder.reverse(); // перевочиваем строку методом Builder-а
+        return (builder.toString()).equals(string);
     }
 }
